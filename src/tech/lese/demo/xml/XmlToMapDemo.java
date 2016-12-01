50352e48170683aca663f529ecc617eeb52dd0d5
lese1000 <598936703@qq.com>
2016-12-01 23:30:33 +0800

    update xmlutils

--- src/tech/lese/demo/xml/XmlToMapDemo.java
+++ src/tech/lese/demo/xml/XmlToMapDemo.java
@@ -16,7 +16,7 @@ public class XmlToMapDemo {
   
 //  FileInputStream fis = new FileInputStream("F:\\devdocs\\DataHub\\UPSAPI\\AccessRequest.xml");
 //  FileInputStream fis = new FileInputStream("F:\\devdocs\\DataHub\\UPSAPI\\ShipmentConfirmRequest.xml");
-  FileInputStream fis = new FileInputStream("F:\\devdocs\\DataHub\\UPSAPI\\book.xml");
+  FileInputStream fis = new FileInputStream("E:\\book.xml");
   byte[] b = new byte[fis.available()];
   fis.read(b);
   String str = new String(b);
@@ -32,6 +32,7 @@ public class XmlToMapDemo {
 //  System.out.println(map.toString());
 //  String xml = XmlUtils.map2XmlWithoutAttr(map, "rootElement");
   String xml = XmlUtils.map2XmlAddAttrType(map, "rootElement");
+  
   System.out.println(xml);
   System.out.println("Use time:"+(System.currentTimeMillis()-beginTime));
 
