e3e4e29d8a15cb4d4f58d3eed86e5ab10dfc552b
lese1000 <598936703@qq.com>
2017-01-22 09:56:56 +0800

    add pdfbox

--- src/tech/lese/demo/httpclient/HttpGetDemo.java
+++ src/tech/lese/demo/httpclient/HttpGetDemo.java
@@ -7,18 +7,28 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
+import tech.lese.demo.common.utils.MD5;
+
 
 public class HttpGetDemo {
 	
 	public static void callGet(){
-		String	uri="http://localhost/huango/api/v1/jifen/consume?phone=18959289898&openid=1111&consumeVal=100&uType=1&orderNum=order11111122333";
+//		String	uri="http://localhost/huango/api/v1/jifen/consume?phone=18959289898&openid=1111&consumeVal=100&uType=1&orderNum=order11111122333";
+		
+		String  uri="http://ispeed.ebit.cn/face/integral/orderByIntegral.jhtml?buyNum=2&ticketType=2&openid=onDC9jseVXywmQL-N1H_R1nMx5XE&sign=";
+		String buyNum="2";
+		String ticketType="2";
+		String openid="onDC9jseVXywmQL-N1H_R1nMx5XE";
+		String sign =MD5.md5Hex(buyNum+openid+ticketType+"jfwx");
+		uri=uri+sign;
+		System.out.println(uri);
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpGet httpGet = new HttpGet(uri);
 		try {
 			CloseableHttpResponse respone = httpclient.execute(httpGet);
 			HttpEntity entity = respone.getEntity();
 			String rtnResult = EntityUtils.toString(entity);
-			System.out.println("callSendTmpMsg >>rtnResult::"+rtnResult);
+			System.out.println(" >>rtnResult::"+rtnResult);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/tech/lese/demo/json/JsonDemo.java
+++ src/tech/lese/demo/json/JsonDemo.java
@@ -9,12 +9,12 @@ public class JsonDemo {
 	public static void stringToJson(){
 		
 		try {
-			Map map = new ObjectMapper().readValue("{\"a\":1, \"b\":2}", HashMap.class);
+			Map map = new ObjectMapper().readValue("{\"a\":1.0000, \"b\":2.03}", HashMap.class);
 //			Map map2 = new ObjectMapper().readValue("{a:1, b:2}", HashMap.class);
 			//开启支持字段名称不带引号的转换模式,默认是关闭的
 //			Map map = new ObjectMapper().configure(Feature.ALLOW_UNQUOTED_FIELD_NAMES, true).readValue("{a:1, b:2}", HashMap.class);  
 			System.out.println("a::"+map.get("a"));
-//			System.out.println("a2::"+map2.get("a"));
+			System.out.println("b::"+map.get("b"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
